dda3e5341751797bd0ffabb411ec5bb16aae6bfe
Adriana <adry.barbato@gmail.com>
2026-04-23 14:15:07 +0200

    ricerca paziente

--- finalcode.py
+++ finalcode.py
@@ -148,7 +148,30 @@ if parametri:
     if back: 
         codice_persona=st.sidebar.text_input("Inserire il codice paziente", placeholder="es: NLS456")
 else: 
+    syn = synapseclient.Synapse()
+    syn.login(authToken=st.session_state.auth_token)
+    folder_file="syn61370558"
+    files=list(syn.getChildren(folder_file))
+    paziente=[child['id'] for child in files]
     codice_persona=st.sidebar.text_input("Inserire il codice paziente", placeholder="es: NLS456")
+    if codice_persona:
+        file_da_aprire=st.selectbox("seleziona il file del soggetto da analizzare", paziente)
+        if file_da_aprire:
+            match = [c['id'] for c in files if c['name'] == file_da_aprire]
+            if match:
+                file_id =match[0] 
+                with st.spinner("Caricamento del file..."):
+                    entità = syn.get(file_id)
+                    df = pd.read_csv(entità.path, sep="," , header=1)
+                    st.success(f"File {file_da_aprire} caricato con successo!")
+                    st.title(f"analisi: {file_da_aprire}")
+                    st.dataframe(df)
+            else:
+                st.error("Nessun file trovato per la prova selezionata")
+        else:
+            st.warning(f"Nessun file trovato per il paziente '{codice_persona}'")
+
+
 
 
    
