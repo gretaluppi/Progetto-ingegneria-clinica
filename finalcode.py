5d02d2712d84d4ffcaa4aba904b0e618e06d834e
Adriana <adry.barbato@gmail.com>
2026-04-23 14:54:26 +0200

    ricerca paziente

--- finalcode.py
+++ finalcode.py
@@ -152,22 +152,24 @@ else:
     syn.login(authToken=st.session_state.auth_token)
     folder_file="syn61370558"
     files=list(syn.getChildren(folder_file))
-    paziente=[child['id'] for child in files]
+    paziente=[child['name'] for child in files]
     codice_persona=st.sidebar.text_input("Inserire il codice paziente", placeholder="es: NLS456")
     if codice_persona:
-        file_da_aprire=st.selectbox("seleziona il file del soggetto da analizzare", paziente)
+        file_scelti= [f for f in paziente if codice_persona in f]
+        if file_scelti:
+            file_da_aprire=st.selectbox("seleziona il file del soggetto da analizzare", file_scelti)
         if file_da_aprire:
             match = [c['id'] for c in files if c['name'] == file_da_aprire]
             if match:
                 file_id =match[0] 
                 with st.spinner("Caricamento del file..."):
                     entità = syn.get(file_id)
-                    df = pd.read_csv(entità.path, sep="," , header=1)
+                    df_paziente = pd.read_csv(entità.path, sep="," , header=1)
                     st.success(f"File {file_da_aprire} caricato con successo!")
                     st.title(f"analisi: {file_da_aprire}")
-                    st.dataframe(df)
+                    st.dataframe(df_paziente)
             else:
-                st.error("Nessun file trovato per la prova selezionata")
+                st.error("ID non trovato per il paziente selezionato")
         else:
             st.warning(f"Nessun file trovato per il paziente '{codice_persona}'")
 
