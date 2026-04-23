f6d6228babf592a5ca3ab6473067aaddb0f8012d
vittoriapaganelli-glitch <vittoriapaganelli@gmail.com>
2026-04-23 13:24:39 +0200

    Adry

--- finalcode.py
+++ finalcode.py
@@ -109,6 +109,45 @@ for element in soggetti_selezionati_updrs:
     else: 
         soggetti_finali_updrs.append({"Subject ID" : element["Subject ID"], "MDS-UPDRS": "Valori Mancanti"})
 
+# ____________________
+# FILTRO PROVA - Adry
+# ____________________
+
+syn = synapseclient.Synapse()
+folder_file="syn61370558"
+files=list(syn.getChildren(folder_file))
+prova=[child['name'] for child in files]
+st.sidebar.header("seleziona tipo di prova")
+selezione=st.sidebar.selectbox("prova eseguita", ["SelfPace","HurriedPace","SelfPace_mat","HurriedPace_mat","SelfPace_matTURN","TandemGait","TUG","Balance","SElfPace_doorpat","FreeWalk"])
+file_scelti= [f for f in prova if selezione in f and (("_mat" in selezione) == ("_mat" in f)) and (("TURN" in selezione) == ("TURN" in f))]
+if file_scelti:
+    file_da_aprire=st.selectbox("seleziona il file del soggetto da analizzare", file_scelti)
+    if file_da_aprire:
+        match = [c['id'] for c in files if c['name'] == file_da_aprire]
+        if match:
+            file_id =match[0] 
+            with st.spinner("Caricamento del file..."):
+                entità = syn.get(file_id)
+                df_prova = pd.read_csv(entità.path, sep="," , header=1)
+                st.success(f"File {file_da_aprire} caricato con successo!")
+                st.title(f"analisi: {file_da_aprire}")
+                st.dataframe(df_prova)
+        else:
+            st.error("Nessun file trovato per la prova selezionata")
+    else:
+        st.warning(f"Nessun file trovato per la prova '{selezione}' (esclusi mat e TURN)")
+    if len(match) > 0:
+            file_id = match[0]
+            entità = syn.get(file_id)
+            df_prova = pd.read_csv(entità.path, sep="," , header=1)
+            st.title("ANALISI DATI PARKINSON")
+            st.dataframe(df_prova)
+    else:
+        st.error("Nessun file trovato per la prova selezionata")
+else:
+    st.info("Per favore, inserisci un nome per accedere ai dati")
+
+
 data_frame_filtrato_eta = pd.DataFrame(soggetti_selezionati_eta)
 st.subheader("Dati filtrati per età")
 st.dataframe(data_frame_filtrato_eta)
