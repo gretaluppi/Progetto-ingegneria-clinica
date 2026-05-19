2e34c4cbacfe442c3b7c1c87c16db592349fbed9
Anna Trombettini <anna.trombettini@studio.unibo.it>
2026-05-19 17:45:56 +0200

    aggiunta grafico EC

--- Multipage_app/pages/5 Motion_variables.py
+++ Multipage_app/pages/5 Motion_variables.py
@@ -34,25 +34,20 @@ def login():
         else: 
             st.error(tf)
 
-# lettura dei due file csv 
-# syn = synapseclient.Synapse() 
-# syn.login(authToken="YOUR_TOKEN_HERE") 
-# PD_mov = synapseutils.syncFromSynapse(syn, 'syn61370558')
-
 st.title("Movements metrics")
 st.divider()
 st.write("TESTO DA SCRIVERE ...")
 
-# Selezione tipologia di grafico da mostrare
+# seleziona dalla sidebar la tipologia di grafico da mostrare
 scelta_gruppo = st.sidebar.radio(
     "Choose one option:",
-    options=["1 specific patient", "2 specific patients", "General trend PD vs. CONTROLS"], 
+    options=["One specific patient", "Two specific patients", "General trend PD vs. CONTROLS"], 
     index=None
 )
 
-#grafico del CoP del paziente x in balance con occhi aperti vs chiusi
-if scelta_gruppo == "1 specific patient":
-    st.subheader("Balance: eyes open vs eyes close")
+#1. grafico del CoP del paziente i in balance con occhi aperti vs chiusi
+if scelta_gruppo == "One specific patient":
+    st.subheader("Patient Balance: CoP Comparison (Eyes Open vs. Closed)")
     codice_paziente=st.text_input("Enter patient ID:", placeholder="es: NLS456")
 
     if codice_paziente:
@@ -74,16 +69,23 @@ if scelta_gruppo == "1 specific patient":
                     df_paziente = pd.read_csv(entità.path, sep=",")
                     st.session_state.df_selezionato = df_paziente # Salviamo il dataframe in session_state per i grafici
                     st.session_state.paziente_corrente = balance_file
-                    st.success(f"{balance_file} read successfully!")
                    
                     st.write("...")
                     df_valido = df_paziente [df_paziente ["GeneralEvent"] != "unlabeled"]
+                    #occhi aperti
                     df_EO = df_valido[df_valido["GeneralEvent"] == "EO_FeetShoWidth"]
                     df_EO = df_EO.copy()  #usiamo .copy() per evitare il Warning di Pandas quando creiamo nuove colonne
                     df_EO["ML"] = (df_EO["RCoP_X"] + df_EO["LCoP_X"]) / 2
                     df_EO["AP"] = (df_EO["RCoP_Y"] + df_EO["LCoP_Y"]) / 2
-                    #df_peso = pd.concat([df_male, df_female], ignore_index = True)
-                    fig = px.line(df_EO, x = "ML", y = "AP", labels={"ML": "CoP in ML direction", "AP": "CoP in AP direction"})
+                    #occhi chiusi
+                    df_EC = df_valido[df_valido["GeneralEvent"] == "EC_FeetShoWidth"]
+                    df_EC = df_EC.copy()
+                    df_EC["ML"] = (df_EC["RCoP_X"] + df_EC["LCoP_X"]) / 2
+                    df_EC["AP"] = (df_EC["RCoP_Y"] + df_EC["LCoP_Y"]) / 2
+                    df_EO["Legend:"] = "Eyes Open" #etichetta per legenda
+                    df_EC["Legend:"] = "Eyes Close"
+                    df_COP = pd.concat([df_EO, df_EC], ignore_index = True)
+                    fig = px.line(df_COP, x = "ML", y = "AP", color = "Legend:", labels={"ML": "CoP in ML direction", "AP": "CoP in AP direction"})
                     st.plotly_chart(fig)
             else:
                 st.error("ID not found for the selected patient.")
@@ -94,45 +96,6 @@ if scelta_gruppo == "1 specific patient":
 
 
 
-
-#st.line_chart(data=None, *, x=None, y=None, x_label=None, y_label=None, color=None, width="stretch", height="content", use_container_width=None)
-
-# if "Test" in scelta_parametri:
-#         syn = synapseclient.Synapse()
-#         syn.login(authToken=st.session_state.auth_token)
-#         folder_file="syn61370558"
-#         all_files_in_folder = list(syn.getChildren(folder_file))
-#         files_disponibili = []
-#         for child in all_files_in_folder:
-#             nome_file= str(child['name']).upper()
-#             for element in soggetti_selezionati_UPDRS:
-#                 if element["Subject ID"] in child['name']:
-#                     files_disponibili.append(child)
-#         st.sidebar.header("select test:")
-#         selezione_prova=st.sidebar.selectbox("test executed", ["SelfPace","HurriedPace","SelfPace_mat","HurriedPace_mat","SelfPace_matTURN","TandemGait","TUG","Balance","SElfPace_doorpat","FreeWalk"])
-#         nomi_disponibili = [f['name'] for f in files_disponibili]
-#         file_scelti = []
-#         for nome in nomi_disponibili:
-#             if selezione_prova in nome and (("_mat" in selezione_prova)==("_mat" in nome)) and (("TURN" in selezione_prova)==("TURN" in nome)):
-#                 file_scelti.append(nome)
-#         if file_scelti:
-#             file_da_aprire=st.selectbox("select the file to analyze", file_scelti)
-#             if file_da_aprire:
-#                 match = [c['id'] for c in files_disponibili if c['name'] == file_da_aprire]
-#                 if match:
-#                     file_id =match[0] 
-#                     with st.spinner("Downloading file..."):
-#                         entità = syn.get(file_id)
-#                         df_prova = pd.read_csv(entità.path, sep="," , header=1)
-#                         st.success(f"File {file_da_aprire} downloaded successfully!")
-#                         st.title(f"File: {file_da_aprire}")
-#                         st.dataframe(df_prova)
-#                 else:
-#                     st.error("No file found for the selected test")
-#             else:
-#                 st.warning(f"No file found for the test '{selezione_prova}' (excluded mat and TURN)")
-#         else:
-#             st.info("No file available")
 
 
 # def momenti_contatto(file): #serve per creare un sottofile con solo la riga e colonne di interesse (cioè colonna del tempo e righe in cui il valore cambia da 0 a 1 o da 1 a 0)
